e7ffdec172516755ba020c9b04488fde0154e75f
Effanuel <32594043+Effanuel@users.noreply.github.com>
2018-05-09 15:38:34 +0300

    Completing Linear Model
    Completed

--- Linear Model.py
+++ Linear Model.py
@@ -25,7 +25,7 @@ img_size = 28
 
 img_size_flat = img_size * img_size
 
-img_shape = (img_size * img_size)
+img_shape = (img_size, img_size)
 
 num_classes = 10
 
@@ -57,7 +57,7 @@ def plot_images(images, cls_true, cls_pred=None):
     # in a single Notebook cell.
     plt.show()
 
-    # Get the first images from the test-set.
+# Get the first images from the test-set.
 images = data.test.images[0:9]
 
 # Get the true classes for those images.
@@ -66,9 +66,165 @@ cls_true = data.test.cls[0:9]
 # Plot the images and labels using our helper-function above.
 plot_images(images=images, cls_true=cls_true)
 
+#Optimizing variables
+x = tf.placeholder(tf.float32, [None, img_size_flat])
+y_true = tf.placeholder(tf.float32, [None, num_classes])
+y_true_cls = tf.placeholder(tf.int64, [None])
+weights = tf.Variable(tf.zeros([img_size_flat, num_classes]))
+biases = tf.Variable(tf.zeros([num_classes]))
+
+
+#Multiplying [num_images, img_size_flat] x [img_size_flat, num_classes]
+logits = tf.matmul(x, weights) + biases
+#Normalization
+y_pred = tf.nn.softmax(logits)
+
+#Getting Predicted class(highest probability)
+y_pred_cls = tf.argmax(y_pred, axis=1)
+
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true)
+#averge of cross_entropy
+cost = tf.reduce_mean(cross_entropy)
+
+#minimizing the average
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.6).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.35).minimize(cost)
+
+
+#check how correct y pred is
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+
+#calculates accuracy
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
 
 
+session = tf.Session()
+session.run(tf.initialize_all_variables())
+
+batch_size = 100
+
+def optimize(num_iterations):
+    for _ in range(num_iterations):
+        
+
+        x_batch, y_true_batch = data.train.next_batch(batch_size)
 
 
+        feed_dict_train = {
+            x: x_batch,
+            y_true: y_true_batch
+            }
+
+        session.run(optimizer, feed_dict=feed_dict_train)
+
+
+feed_dict_test = {
+    x: data.test.images,
+    y_true: data.test.labels,
+    y_true_cls: data.test.cls
+}
+
+def print_accuracy():
+    acc = session.run(accuracy, feed_dict=feed_dict_test)
+
+    print("Accuracy on the test set: {0:.1%}".format(acc))
+
+
+def print_confusion_matrix():
+    # Get the true classifications for the test-set.
+    cls_true = data.test.cls
+    
+    # Get the predicted classifications for the test-set.
+    cls_pred = session.run(y_pred_cls, feed_dict=feed_dict_test)
+
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=cls_true,
+                          y_pred=cls_pred)
+
+    # Print the confusion matrix as text.
+    print(cm)
+
+    # Plot the confusion matrix as an image.
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+
+    # Make various adjustments to the plot.
+    plt.tight_layout()
+    plt.colorbar()
+    tick_marks = np.arange(num_classes)
+    plt.xticks(tick_marks, range(num_classes))
+    plt.yticks(tick_marks, range(num_classes))
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
+def plot_example_errors():
+    # Use TensorFlow to get a list of boolean values
+    # whether each test-image has been correctly classified,
+    # and a list for the predicted class of each image.
+    correct, cls_pred = session.run([correct_prediction, y_pred_cls],
+                                    feed_dict=feed_dict_test)
+
+    # Negate the boolean array.
+    incorrect = (correct == False)
+    
+    # Get the images from the test-set that have been
+    # incorrectly classified.
+    images = data.test.images[incorrect]
+    
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = data.test.cls[incorrect]
+    
+    # Plot the first 9 images.
+    plot_images(images=images[0:9],
+                cls_true=cls_true[0:9],
+                cls_pred=cls_pred[0:9])
+
+
+
+def plot_weights():
+    # Get the values for the weights from the TensorFlow variable.
+    w = session.run(weights)
+    
+    # Get the lowest and highest values for the weights.
+    # This is used to correct the colour intensity across
+    # the images so they can be compared with each other.
+    w_min = np.min(w)
+    w_max = np.max(w)
+
+    # Create figure with 3x4 sub-plots,
+    # where the last 2 sub-plots are unused.
+    fig, axes = plt.subplots(3, 4)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Only use the weights for the first 10 sub-plots.
+        if i<10:
+            # Get the weights for the i'th digit and reshape it.
+            # Note that w.shape == (img_size_flat, 10)
+            image = w[:, i].reshape(img_shape)
+
+            # Set the label for the sub-plot.
+            ax.set_xlabel("Weights: {0}".format(i))
+
+            # Plot the image.
+            ax.imshow(image, vmin=w_min, vmax=w_max, cmap='seismic')
+
+        # Remove ticks from each sub-plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+print_accuracy()
